38f6714f7de188d9371f89831f56b7494705442d
drianarifi <da73264@ubt-uni.net>
2025-12-05 19:00:39 +0100

    Completed the HomePage for MJ Airlines

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,14 @@
 
+
+    const slides = document.querySelectorAll('.slide');
+    let currentIndex = 0;
+
+    function changeSlide(direction) {
+      currentIndex += direction;
+      if (currentIndex < 0) {
+        currentIndex = slides.length - 1;
+      } else if (currentIndex >= slides.length) {
+        currentIndex = 0;
+      }
+      document.getElementById('slides').style.transform = `translateX(-${currentIndex * 100}%)`;
+    }
